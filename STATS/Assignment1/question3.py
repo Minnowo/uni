540b8adab024c63396b9c308969f380728af60fe
Alice <62451415+Minnowo@users.noreply.github.com>
2022-09-15 13:14:50 -0400

    stats

---
+++ STATS/Assignment1/question3.py
@@ -0,0 +1,48 @@
+
+
+data = """SSSS
+SSSF
+SSFS
+SSFF
+SFSS
+SFSF
+SFFS
+SFFF
+FSSS
+FSSF
+FSFS
+FSFF
+FFSS
+FFSF
+FFFS
+FFFF"""
+
+total = 0
+nyah = dict()
+
+print("likely of each combination")
+for i in data.split("\n"):
+
+    t = 1
+
+    for j in i:
+
+
+        if j == "F":
+            t *= 0.6
+
+        if j == "S":
+            t *= 0.4
+
+    print(i, t)
+    if "".join(sorted(i)) not in nyah:
+        nyah["".join(sorted(i))] = [] 
+
+    nyah["".join(sorted(i))].append(t)
+    total += t 
+
+print()
+print("distribution")
+
+for i, k in nyah.items():
+    print(i, sum(k)*100)
