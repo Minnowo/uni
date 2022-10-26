bf69e26e4ee53c7f684e88118916d00f972b7e72
minnowo <62451415+Minnowo@users.noreply.github.com>
2022-10-25 23:21:44 -0400

    stats assignment 3 and some data analysis practice with numpy

---
+++ STATS/Assignment3/question2.py
@@ -0,0 +1,28 @@
+
+import math
+
+
+n = 15 
+
+x = 1425 
+y = 10.68 
+xx = 139_037.25
+yy = 7.8518
+xy = 987.645 
+
+sxy = xy - (x * y) / n
+sxx = xx - (math.pow(x,2) / n)
+syy = yy - (math.pow(y,2) / n)
+
+r = sxy / (math.sqrt(sxx) * math.sqrt(syy))
+
+b = sxy / sxx 
+avg_y = y / n
+avg_x = x / n 
+
+a = avg_y - b*avg_x
+
+print(f"a. line = {a} + {b} X")
+
+
+print(f"b. line = {a} + {b} X")
